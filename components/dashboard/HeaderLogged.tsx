05682c6b532bfc56b5d6eadab46a7749825ded40
Samuel Barubiriza <samuel.barubiriza@abtatsy.com>
2026-05-07 02:56:47 +0200

    adjust sidebart

--- components/dashboard/HeaderLogged.tsx
+++ components/dashboard/HeaderLogged.tsx
@@ -4,7 +4,7 @@ import { SidebarTrigger } from "../ui/sidebar";
 export function HeaderLogged({ userName }: { userName: string | null | undefined }) {
     return (
         <header className="flex justify-between w-full px-5 bg-white">
-            <SidebarTrigger className="my-4" />
+            <SidebarTrigger className="my-4 text-2xl" />
 
             <div className="flex items-center gap-2 my-5">
                 <UserCircle2 />
